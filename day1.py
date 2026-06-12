7e849093950d9a1dd985f255b1ad95cde12e4ae3
hitanshi64 <hitanshiaasnani4@gmail.com>
2026-06-12 18:49:16 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,26 @@
+print("hello world")
+
+print("My Name Is Hitanshi")
+
+print(20+30)
+
+print("I Am Learning Python")
+
+#basic calculation
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+
+
+# My first comment
+print("Hello")
+
+
+#Escape Character (\n)
+print("Hitanshi\nAasnani")
+
+
+print("Name:Hitanshi")
+print("city:surat")
+print("course:python")
