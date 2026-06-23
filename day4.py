f00c5d81181158084864d4d44cb8a3984fcb2720
hitanshi64 <hitanshiaasnani4@gmail.com>
2026-06-23 18:36:43 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,39 @@
+name =input("enter your name:")
+print("hello",name)
+
+age= input("enter your age:")
+print(age)
+print(type(age))
+
+age= int(input("enter your age:"))
+print(age)
+print(type(age))
+
+marks = float(input("enter your marks:"))
+print(marks)
+print(type(marks))
+
+#Name Greeting Program
+name = input("Enter your name: ")
+
+print(f"Hello {name}, Welcome to Python!")
+
+#Age Calculator
+birth_year=int(input("enter your birth year:"))
+age = 2026- birth_year
+print("your age is :",age)
+
+#Sum of Two Numbers
+
+num1= int(input("enter any number:"))
+num2= int(input("enter any number:"))
+
+print("sum of two numbers:",num1+num2)
+
+
+
+#User Information
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+
+print(f"My name is {name} and I live in {city}.")
